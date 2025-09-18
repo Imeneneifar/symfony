1200a0e541572ff6f0574f088eabb674498d3826
imen <imen.neifar@esprit.tn>
2025-09-18 08:43:26 +0100

    Initial commit

---
+++ src/Controller/WelcomecontrollerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use function PHPUnit\Framework\returnArgument;
+
+final class WelcomecontrollerController extends AbstractController
+{
+    #[Route('/welcomecontroller', name: 'app_welcomecontroller')]
+    public function index(): Response
+    {
+        return $this->render('welcomecontroller/index.html.twig', [
+            'controller_name' => 'Welcome_Controller', 'classe' =>'3A25'
+        ]);
+    }
+
+    #[Route('/hello/{id}', name: 'hello')]
+
+    public function show($id): Response
+    {
+        return $this->render('welcomecontroller/index.html.twig', ['ident' => $id]) ;
+    }
+
+}
